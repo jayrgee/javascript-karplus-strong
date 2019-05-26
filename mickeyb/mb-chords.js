069b894f931cf3a55d713cc123f326262ac7aec9
jayrgee <jayrgee@fastmail.fm>
2019-05-27 00:53:17 +1000

    add initial mb files

---
+++ mickeyb/mb-chords.js
@@ -0,0 +1,44 @@
+(function(mickeyb) {
+  mickeyb.Chords = {
+    '1': {
+      name: 'G',
+      notes: [3,5,5,4,3,3]
+    },
+    '2': {
+      name: 'Gma7',
+      notes: [3,-1,4,4,3,-1]
+    },
+    '3': {
+      name: 'Gma6',
+      notes: [3,-1,2,4,3,-1]
+    },
+    '4': {
+      name: 'Ami7',
+      notes: [5,-1,5,5,5,5]
+    },
+    '5': {
+      name: 'Ami6',
+      notes: [5,-1,4,5,5,5]
+    },
+    '6': {
+      name: 'D13♭5♭9',
+      notes: [3,5,-1,4,6,6]
+    },
+    '7': {
+      name: 'Gma7',
+      notes: [3,5,-1,4,7,7]
+    },
+    'n': {
+      name: '',
+      notes: []
+    },
+    'sharp': {
+      name: '♯',
+      notes: []
+    },
+    'flat': {
+      name: '♭',
+      notes: []
+    }
+  };
+})((window.MickeyB = window.MickeyB || {}));
